8aa5547445a6f8d7cba9273b6dc028c0bf35d13a
Josh Shepherd <josh_lmao@live.co.uk>
2024-06-22 21:30:38 +0100

    CommonUI setup and groundwork

--- Source/UE5EnhancedInputEx/UE5EnhancedInputEx.Build.cs
+++ Source/UE5EnhancedInputEx/UE5EnhancedInputEx.Build.cs
@@ -7,17 +7,33 @@ public class UE5EnhancedInputEx : ModuleRules
 	public UE5EnhancedInputEx(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+            "CommonGame",
+            "CommonUI",
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "GameplayTags",
+            "InputCore",
+            "ModularGameplayActors",
+            "UMG",
+        });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        // Uncomment if you are using Slate UI
+        PrivateDependencyModuleNames.AddRange(new string[]
+        {
+            "Slate",
+            "SlateCore"
+        });
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
 }
