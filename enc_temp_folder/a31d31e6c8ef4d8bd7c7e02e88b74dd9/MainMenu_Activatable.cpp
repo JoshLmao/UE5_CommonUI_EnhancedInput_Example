4dd5d69a6921da4d9fe5680edf16a79112839b1e
Josh Shepherd <josh_lmao@live.co.uk>
2024-06-22 22:11:30 +0100

    MainMenu custom activatable setup

---
+++ enc_temp_folder/a31d31e6c8ef4d8bd7c7e02e88b74dd9/MainMenu_Activatable.cpp
@@ -0,0 +1,6 @@
+#include "MainMenu_Activatable.h"
+
+UMainMenu_Activatable::UMainMenu_Activatable()
+{
+	bSupportsActivationFocus = true;
+}
---
+++ enc_temp_folder/c6a98ff4625dd9365c9ff177739f9d1/MainMenu_Activatable.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <CoreMinimal.h>
+
+#include "CommonActivatableWidget.h"
+
+#include "MainMenu_Activatable.generated.h"
+
+UCLASS(Abstract)
+class UMainMenu_Activatable : public UCommonActivatableWidget
+{
+	GENERATED_BODY()
+
+public:
+	UMainMenu_Activatable();
+};
